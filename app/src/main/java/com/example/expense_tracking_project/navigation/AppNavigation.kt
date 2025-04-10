4352651cb462af420b7c1763901b494ea753258c
RaghadAlmehmadi <raghadalharbi078@gmail.comraghadalharbi078@gmail.com>
2025-04-10 12:48:15 +0300

    Design reset password screens,nav app

--- app/src/main/java/com/example/expense_tracking_project/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/expense_tracking_project/navigation/AppNavigation.kt
@@ -1,2 +1,29 @@
 package com.example.expense_tracking_project.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.expense_tracking_project.presentation.ui.resetPassword.CheckEmailScreen
+import com.example.expense_tracking_project.presentation.ui.resetPassword.Login
+import com.example.expense_tracking_project.presentation.ui.resetPassword.ResetPasswordScreen
+import com.example.expense_tracking_project.presentation.ui.resetPassword.Screen
+
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+    NavHost(navController, startDestination = Screen.ResetPassword.route) {
+        composable(Screen.ResetPassword.route) {
+            ResetPasswordScreen(navController)
+        }
+
+        composable(Screen.CheckEmail.route) {
+            CheckEmailScreen(navController)
+        }
+        composable(Screen.log.route) {
+            Login(navController)
+        }
+
+    }
+}
