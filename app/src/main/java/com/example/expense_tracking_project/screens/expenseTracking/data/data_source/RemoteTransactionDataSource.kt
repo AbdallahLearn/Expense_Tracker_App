fa374d485e7f4b53b66659204af65cdc8b3c0bc9
etharalrehaili <85852139+etharalrehaili@users.noreply.github.com>
2025-04-26 22:12:52 +0300

    delete unnecessary classes

--- app/src/main/java/com/example/expense_tracking_project/screens/expenseTracking/data/data_source/RemoteTransactionDataSource.kt
+++
@@ -1,8 +0,0 @@
-//package com.example.expense_tracking_project.screens.expenseTracking.data.data_source
-//
-//import com.example.expense_tracking_project.core.local.entities.Transaction
-//
-//interface RemoteTransactionDataSource {
-//    suspend fun insertTransaction(transaction: Transaction)
-//    suspend fun getAllTransactions(): List<Transaction>
-//}
